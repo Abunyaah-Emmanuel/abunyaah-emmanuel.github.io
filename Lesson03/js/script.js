9be7bbf8c2d97877c92afb0030b8aaf3f8003af4
Abunyaah-Emmanuel <romeolove10061@hotmail.com>
2020-05-10 02:09:52 +0000

    update

--- Lesson03/js/script.js
+++ Lesson03/js/script.js
@@ -1,3 +1,16 @@
 document.getElementById('currentYear').innerHTML = new Date().getFullYear();
 
 document.getElementById("lastUpdate").innerHTML = document.lastModified;
+
+var accordion = document.getElementsByClassName('accordion');
+for(var i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+        this.classList.toggle("accordion-active");
+        var panel = this.nextElementSibling;
+        if(panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
