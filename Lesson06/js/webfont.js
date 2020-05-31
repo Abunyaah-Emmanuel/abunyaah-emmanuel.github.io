b4bad0fd8a8bf439201c05ed9c0629ae63df9452
Abunyaah-Emmanuel <romeolove10061@hotmail.com>
2020-05-31 18:08:05 +0000

    commit

---
+++ Lesson06/js/webfont.js
@@ -0,0 +1,14 @@
+//WebFont Js
+WebFont.load({
+    google: {
+      families: [
+         'Ubuntu',
+         'Ubuntu Condensed',
+         'Lobster',
+         'Fjalla One',
+         'Verdana',
+         'Lucida Sans Unicode',
+         'sans-serif',
+      ]
+    }
+});
