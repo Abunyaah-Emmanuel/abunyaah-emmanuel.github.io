1c98f63c06f6cd5e850ad057ab1673d800192077
Abunyaah-Emmanuel <romeolove10061@hotmail.com>
2020-07-05 00:21:53 +0000

    commit

---
+++ Lesson10/js/webfont.js
@@ -0,0 +1,13 @@
+//WebFont Js
+WebFont.load({
+    google: {
+      families: [
+         'Anton',
+         'Barlow',
+         'Fjalla One',
+         'Lobster',
+         'Ubuntu',
+         'Ubuntu Condensed'
+      ]
+    }
+});
