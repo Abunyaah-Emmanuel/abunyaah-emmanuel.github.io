bfbc3b5cb5299bf90fa35a547b363ad00a9e4496
Abunyaah-Emmanuel <romeolove10061@hotmail.com>
2020-07-05 05:02:14 +0000

    commit

--- Lesson10/js/weatherapi.js
+++
@@ -1,8 +0,0 @@
-
-
-const apiURL = "https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=439d4b804bc8187953eb36d2a8c26a02";
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-  });
